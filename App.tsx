d2390529a8fafa864dfdff9af5681f6857368616
fatima alzahraa almaz <almazfatima9@gmail.com>
2025-06-07 17:04:14 +0300

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,181 @@
+import { useEffect, useRef, useState } from 'react'
+ 
+import './App.css'
+import type React from 'react';
+
+   const songs=[
+    {
+name:'Oshi No Ko (IDOL)',
+imgurl:'./public/oshi no ko.jpg',
+audiourl:'./public/oshi no ko.mp3'
+},
+{
+name:'Fly Me To The Moon',
+imgurl:'./public/fly me to the moon.jpg',
+audiourl:'https://github.com/fatimaAlzahraa-almaz/music-play/raw/main/fly%20me%20to%20the%20moon.mp3'
+},
+{
+name:'Breath-Years & Years',
+imgurl:'./public/breath.jpg',
+audiourl:'https://github.com/fatimaAlzahraa-almaz/music-play/raw/main/breath.mp3'
+},
+{
+name:'Dark Opening',
+imgurl:'./public/dark.png',
+audiourl:'https://github.com/fatimaAlzahraa-almaz/music-play/raw/main/Dark.mp3'
+}
+ ,
+ {
+name:'Rose-APT',
+imgurl:'./public/apt.jpg',
+audiourl:'https://github.com/fatimaAlzahraa-almaz/music-play/raw/main/Rose-Apt.mp3'
+}
+,
+ 
+{
+name:'Call Of Silence -AOT',
+imgurl:'./public/call of silence.jpg',
+audiourl:'https://github.com/fatimaAlzahraa-almaz/music-play/raw/main/call%20of%20silence.mp3'
+},
+{
+name:'Round and Round',
+imgurl:'./public/round.jpg',
+audiourl:'https://github.com/fatimaAlzahraa-almaz/music-play/raw/main/Round%20and%20Round.mp3'
+},
+{
+name:'Mortals Funk (Remix)',
+imgurl:'./public/mortall funk.jpg',
+audiourl:'https://github.com/fatimaAlzahraa-almaz/music-play/raw/main/Mortals%20Funk%20Remix.mp3'
+},{
+name:'2 Phut Hon Funk',
+imgurl:'./public/phot hon.jpg',
+audiourl:'./public/2 Phut Hon Funk.mp3'
+},
+{
+name:'Hurt-maikubi',
+imgurl:'./public/hurt.jpg',
+audiourl:'./public/hurt maikubi.mp3'
+},
+ 
+ 
+
+];
+ 
+function App() {
+  
+    const audioRef = useRef<HTMLAudioElement>(null);
+    const[index,setindex]=useState(0);
+    const [progress, setProgress] = useState(0);
+    const[isplay,setplay]=useState(false);
+    const[imgplayer,setimgplayer]=useState('public/Frame 3.png');
+    
+    
+    useEffect(() => {
+        const audio = audioRef.current;
+
+        if (audio) {
+            const updateProgress = () => {
+                const progressValue = (audio.currentTime / audio.duration) * 100;
+                setProgress(progressValue);
+            };
+ const handleEnded = () => {
+                setindex((prevIndex) => (prevIndex + 1) % songs.length);
+            };
+
+            audio.addEventListener('timeupdate', updateProgress);
+            audio.addEventListener('ended', handleEnded);
+            return () => {
+                audio.removeEventListener('timeupdate', updateProgress);
+                audio.removeEventListener('ended', handleEnded);
+            };
+        }
+    }, []);
+     useEffect(() => {
+        if (audioRef.current) {
+            audioRef.current.src = songs[index].audiourl;
+            if(isplay)
+                audioRef.current.play();
+            
+        }
+    }, [index]);
+
+    const handleProgressChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const newTime = (parseFloat(event.target.value) / 100) * (audioRef.current?.duration || 0);
+        if (audioRef.current) {
+            audioRef.current.currentTime = newTime;
+        }
+    };
+ function hundleplay(play=false)
+ {
+if(play)
+{
+    setplay(false);
+    audioRef.current?.pause();
+    setimgplayer('./public/Frame 3.png')
+
+}
+else{
+    setplay(true);
+    audioRef.current?.play();
+    setimgplayer('./public/stopmusic.png')
+
+}
+ }
+  
+ 
+ function hundelright()
+ {
+setindex((index+1)%songs.length);
+
+
+
+
+ }
+ function hundleleft()
+ {
+  setindex((index+(songs.length-1))%songs.length);
+ }
+
+
+  return (
+    < div className="wrap"> 
+    <div className='container'> 
+        <div className="head">
+             <div className='dodev' ><img className="doimg"src='./do.jpg'/></div>
+             <div className='songname'><h3 className='hname'>{songs[index].name}</h3> 
+               </div>
+             <div className='dodev' ><img className="doimg"src='./do.jpg'/></div>
+        </div>
+      
+      <div className='imgdiv'><img className='imgsong' src={songs[index].imgurl}/></div>
+            <div>
+            <audio ref={audioRef} className='audio'  />
+            <input
+            className='inputclass'
+                type="range"
+                id="progressBar"
+                min="0"
+                value={progress}
+                step="1"
+                onChange={handleProgressChange}
+                 
+            />
+             </div>
+
+            <div className="btnsbox">
+  <button onClick={hundleleft} className='btn leftr'><img className="btnimg leftrimg"src="./public/leftbt.png"/></button>
+  
+  <button  className='btn' onClick={()=>hundleplay(isplay)}><img className="btnimg" src={imgplayer}/></button>
+  <button  className='btn leftr' onClick={hundelright}><img className="btnimg leftrimg" src="./public/rightbt 4.png"/></button>
+  
+</div>
+  
+
+     </div>  
+     </div> 
+    );
+
+  
+}
+
+export default App
